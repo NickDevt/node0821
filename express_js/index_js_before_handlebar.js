c2059a632d62f952c23795284f83eeca22e3f2da
NickDevt <48755888+NickDevt@users.noreply.github.com>
2021-08-31 18:21:19 +0300

    lesson 3-23

---
+++ express_js/index_js_before_handlebar.js
@@ -0,0 +1,33 @@
+//уроки 3_18, 3_19
+// Core функционал подключения
+// Данный index js работает с файлами из папки early_html_for_first_server_config
+const express = require('express')
+const path = require('path')
+// В экспрессе сервер создается так
+const app = express()
+
+
+
+//В отличии от базового http функционала - express делает много стандартных вещей из коробки
+// Например он прописывает заголовки, контент тайп итд
+app.get('/', (req, res) => {
+    // res.status(200)
+    res.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+app.get('/about', (req, res) => {
+    // res.status(200)
+    res.sendFile(path.join(__dirname, 'views ', 'about.html'))
+})
+
+
+
+//Если порт не определен - значение по умолчанию
+const PORT = process.env.PORT || 3000
+
+
+app.listen(PORT, ()=> {
+    console.log(`Server is running on port ${PORT}`)
+})
+
+
