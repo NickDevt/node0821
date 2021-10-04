a7f262cf4f91a1b07c0a4b65238438617b2a57cc
NickDevt <48755888+NickDevt@users.noreply.github.com>
2021-10-04 19:14:13 +0300

    вывод данных при помощи sequelize

--- protoTable1/db.orm.js
+++ protoTable1/db.orm.js
@@ -5,14 +5,27 @@
 // https://www.youtube.com/watch?v=PS_vWDxwATM
 
 
-
+// Дальше я создаю модели всех таблиц
+// Затем я экспортирую их
+// В файле models я создаю класс, который производит с этими таблицами конкретные действия
+// То есть модели у меня есть, а оперирую ими я уже в спец классе в models
+// Таким образом будет несколько классов, которые будут делать несколько действий
+// Что касается импорта - надо искать в документации
 
 const {Sequelize, DataTypes, Model} = require('sequelize')
 
-const sequelize = new Sequelize('postgres', 'postgres', 'root', {
-    host: '127.0.0.1',
+// const sequelize = new Sequelize('postgres', 'postgres', 'root', {
+//     host: '127.0.0.1',
+//     dialect: 'postgres'
+// })
+
+const sequelize = new Sequelize('umbmvdqn', 'umbmvdqn', 'SFVCdUDKEIBtkWDPmTjar5PgHiK1SldU', {
+    host: 'hattie.db.elephantsql.com',
     dialect: 'postgres'
 })
+
+
+
 
 // console.log(sequelize)
 
@@ -26,9 +39,28 @@ Contact.init(
     },
     {
         sequelize,
-        modelName: 'contacts',
+        modelName: 'contact',
         timestamps:false}
 )
+
+
+class Region extends Model{}
+
+Region.init(
+    {
+        id: {type: DataTypes.INTEGER , autoIncrement: true, primaryKey: true, allowNull: false, unique:true },
+        name: {type: DataTypes.STRING, allowNull: false}
+    },
+    {
+        sequelize,
+        modelName: 'Region',
+        tableName: 'Region',
+        timestamps:false}
+)
+
+
+
+
 
 
 
@@ -53,10 +85,27 @@ Contact.init(
 //
 
 
+// Такой вариант работает:
+//     class Contact extends Model{}
+//
+// Contact.init(
+//     {
+//         id: {type: DataTypes.INTEGER , autoIncrement: true, primaryKey: true, allowNull: false, unique:true },
+//         name: {type: DataTypes.STRING, allowNull: false}
+//     },
+//     {
+//         sequelize,
+//         modelName: 'contacts',
+//         timestamps:false}
+// )
+
 
 async function test(){
     const d = await Contact.findAll()
-    console.log(d)
+    const p = await Region.findAll()
+    // console.dir(d)
+    console.log(JSON.stringify(d, null, 2))
+    console.log(JSON.stringify(p, null, 2))
 }
 
 test()
